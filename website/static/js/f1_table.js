298e4fe40efe2e1414d2f4e6c5ff19771a7d6046
Jonathan Dan <jonathan.dan@epfl.ch>
2025-04-08 23:16:58 +0200

    Standardize dataset names

--- website/static/js/f1_table.js
+++ website/static/js/f1_table.js
@@ -88,7 +88,7 @@ async function fetchTrainingDatasets(algorithm) {
                 else if (dataset === 'Physionet Siena Scalp EEG'){
                     return 'siena';
                 }
-                else if (dataset === 'Temple University Seizure Corpus'){
+                else if (dataset === 'TUH EEG Seizure Corpus'){
                     return 'tuh';
                 }
                 else if (dataset === 'KU Leuven SeizeIT1'){
